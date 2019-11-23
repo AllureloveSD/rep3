ab533b8acdeef36b9e74d695a97a7236033923b0
Allurelove <843505499@qq.com>
2019-11-23 19:33:34 +0800

    第一次提交

--- day22_git/src/com/itheima/demo/Git_demo.java
+++ day22_git/src/com/itheima/demo/Git_demo.java
@@ -3,5 +3,6 @@ package com.itheima.demo;
 public class Git_demo {
     public static void main(String[] args) {
         System.out.println("幸得识卿桃花面,自此阡陌多暖春");
+        System.out.println("青青陌上桑,春草年年绿");
     }
 }
